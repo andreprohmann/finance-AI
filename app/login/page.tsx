1ebcb4bf5f7e005cf1d7410319cb5f9e3f19940a
Andre Prohmann <98752890+andreprohmann@users.noreply.github.com>
2024-11-06 11:59:28 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-const LoginPage = () => {
+
+const LoginPage = async () => {
+
+    const { userId } = await auth();
+    if (userId) {
+        redirect('/');
+    }
+
     return (
         <div className="h-full grid grid-cols-2">
             {/* ESQUERDA */}
@@ -13,10 +23,13 @@ const LoginPage = () => {
                 <p className="text-muted-foreground mb-8">
                     A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
                 </p>
-                <Button variant={"outline"}>
-                    <LogInIcon className="mr-2" />
-                    Fazer Login ou Criar Conta
-                </Button>
+                <SignInButton>
+                    <Button variant={"outline"}>
+                        <LogInIcon className="mr-2" />
+                        Fazer Login ou Criar Conta
+                    </Button>
+                </SignInButton>
+
             </div>
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth()
+  if (!userId) {
+    redirect('/login');
+  }
   return (
-    <div className="flex items-center justify-center w-screen dark">
-      <Button />
+
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName appearance={{
+        baseTheme: dark,
+      }} />
+
+
     </div>
+
 
 
   );
 }
+export default Home;
