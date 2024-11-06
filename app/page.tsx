f599f01fea39ff549102d8d360add612208c05a4
Andre Prohmann <98752890+andreprohmann@users.noreply.github.com>
2024-11-06 11:01:08 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center w-screen">
-      <h1 className="text-red-500">Hello World!</h1>
+      <Button />
     </div>
 
 
